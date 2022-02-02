2930decc0f64a4d6a9ccc0979bb91e08987a3792
YaroslavLazorenko <lazorenko85@gmail.com>
2022-02-02 17:24:05 +0200

    Add private and public routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { ROUTES } from './consts';
+import { authOperations } from 'redux/auth';
 import AppBar from './components/AppBar';
 import RegisterPage from './pages/RgisterPage';
 import LoginPage from './pages/LoginPage';
 import ContactsPage from './pages/ContactsPage';
-import { authOperations } from 'redux/auth';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from 'components/PublicRoute';
 import './App.css';
 
 const App = () => {
@@ -21,9 +23,30 @@ const App = () => {
       <AppBar />
       <main>
         <Routes>
-          <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
-          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-          <Route path={ROUTES.CONTACTS} element={<ContactsPage />} replace />
+          <Route
+            path={ROUTES.REGISTER}
+            element={
+              <PublicRoute redirectTo={ROUTES.CONTACTS} restricted>
+                <RegisterPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path={ROUTES.LOGIN}
+            element={
+              <PublicRoute redirectTo={ROUTES.CONTACTS} restricted>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path={ROUTES.CONTACTS}
+            element={
+              <PrivateRoute redirectTo={ROUTES.LOGIN}>
+                <ContactsPage />
+              </PrivateRoute>
+            }
+          />
           <Route path="/*" element={<Navigate to={ROUTES.LOGIN} replace />} />
         </Routes>
       </main>
